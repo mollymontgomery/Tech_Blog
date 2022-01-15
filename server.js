a448c4e5616b8682a9fd0afdc30e7640cb41d7aa
Molly Montgomery <Molly@mollys-air-2.lan>
2022-01-15 14:39:12 -0600

    added controllers folder and files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const routes = require('./controllers/');
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
